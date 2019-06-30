230aad292bb675746d96df6c8e56fde09730906f
iSlidex <45302075+iSlidex@users.noreply.github.com>
2019-06-29 22:02:57 -0400

    cambio manual

--- Main/dbProv/modprov.php
+++ Main/dbProv/modprov.php
@@ -12,16 +12,11 @@ $phone = $_POST['phone'];
 $rif = $_POST['nextrif'];
 $prev = $_POST['rif'];
 
-$sql1 = "SELECT nombre FROM providers WHERE rif='$prev' limit 1";
-$result = mysql_query($sql1);
-$value = mysql_fetch_object($result);
-$provAnt = $value->nombre;
-
+$result = $conn->query("SELECT nombre FROM providers WHERE rif='$prev' limit 1")->fetch_object()->nombre; 
 $sql = "UPDATE providers SET nombre = '$name', direccion = '$dir', telefono = '$phone',
 rif = '$rif' WHERE rif='$prev'";
-$sql2 = "UPDATE users SET Proveedor='$name' WHERE Proveedor=$provAnt" ;
-
-if ($conn->query($sql) === TRUE) {
+$sql2 = "UPDATE products SET Proveedor='$name' WHERE Proveedor='$result'" ;
+if ($conn->query($sql) && $conn->query($sql2) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
